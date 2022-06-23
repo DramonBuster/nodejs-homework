b6f3128d37dc2358d35577e5ce4a6b4f4b0dc93d
DramonBuster <terradestroyman@ukr.net>
2022-06-23 19:32:03 +0300

    Додана обробка jimp

--- controllers/users/updateAvatar.js
+++ controllers/users/updateAvatar.js
@@ -1,29 +1,29 @@
 const { User } = require("../../models")
 const path = require("path")
 const fs = require("fs/promises")
-// const Jimp = require("jimp");
+const Jimp = require("jimp");
 
 const avatarsDir = path.join(__dirname, "../../", "public", "avatars")
 
 const updateAvatar = async (req, res) => {
-    const { path: tempUpload, originalname } = req.file;
+    const { path: tmpUpload, originalname } = req.file;
     const { _id: id } = req.user;
     const imageName = `${id}_${originalname}`;
     try {
         const resultUpload = path.join(avatarsDir, imageName);
-        await fs.rename(tempUpload, resultUpload);
+        await fs.rename(tmpUpload, resultUpload);
         const avatarURL = path.join("public", "avatars", imageName);
         // "public/avatars/my-avatar.png"
 
-        // const avatar = await Jimp.read(avatarURL);
-        // await avatar.autocrop().cover(250, 250, Jimp.HORIZONTAL_ALIGN_CENTER | Jimp.VERTICAL_ALIGN_MIDDLE).quality(60).writeAsync(avatarURL);
+        const avatar = await Jimp.read(avatarURL);
+        await avatar.autocrop().cover(250, 250, Jimp.HORIZONTAL_ALIGN_CENTER | Jimp.VERTICAL_ALIGN_MIDDLE).quality(60).writeAsync(avatarURL);
 
         await User.findByIdAndUpdate(req.user._id, { avatarURL })
         res.status(200).json({
             avatarURL
         })
     } catch (error) {
-        await fs.unlink(tempUpload);
+        await fs.unlink(tmpUpload);
         throw error;
     }
 }
